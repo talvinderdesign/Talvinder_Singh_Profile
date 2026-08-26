4be3d57a859c379b8f757beba90a0ff5060f7516
talvinderdesign <talvinder8689@gmail.com>
2026-08-26 23:50:24 +0530

    vv

--- server.ts
+++ server.ts
@@ -5,7 +5,7 @@ import { PERSONAL_INFO, EXPERIENCES, EY_BADGES, EY_AWARDS, SKILL_CATEGORIES } fr
 
 async function startServer() {
   const app = express();
-  const PORT = 3000;
+  const PORT = Number(process.env.PORT) || 3000;
 
   app.use(express.json());
 
